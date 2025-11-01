7d03e55b3b16aaa970f47c4d6915b400a84ee462
Ellen Ruturi <ellenruturi@gmail.com>
2025-11-01 18:06:03 +0200

    Add react-icons dependency and update Footer component with social media links

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Footer.css";
+import { FaFacebook,FaInstagram, FaTwitter } from "react-icons/fa";
+
 
 function Footer() {
   return (
     <footer className="footer">
-     <img src="footer-logo.png" alt="Little Lemon Logo" />
+     <img src="/ll logo2.png" className="main-logo" alt="Little Lemon Logo" />
         <h4>Navigation</h4>
          <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Menu</a></li>
-          <li><a href="#">Reservations</a></li>
-          <li><a href="#">Order Online</a></li>
-          <li><a href="#">Login</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/reservations">Reservations</a></li>
+          <li><a href="/order-online">Order Online</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
         <h4>Contact Us</h4>
         <p>1234 Lemon St, Chicago, IL 60601</p>
@@ -21,9 +23,26 @@ function Footer() {
 
         <h4>Follow Us</h4>
         <ul>
-          <li><a href="#">Facebook</a></li>
-          <li><a href="#">Instagram</a></li>
-          <li><a href="#">Twitter</a></li>
+          <li> <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Facebook"
+        >
+          <FaFacebook />
+        </a></li>
+          <li><a  href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram /></a></li>
+          <li><a  href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaTwitter /></a></li>
         </ul>
 
         <p>&copy; 2024 Little Lemon. All rights reserved.</p>
